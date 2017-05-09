aa3468340b502ec6a3266b64580af057bb9fc689
Jenny <kang.je@gmail.com>
2017-05-08 17:53:54 -0700

    fleshed out skeleton of code more; need to work on regional work

--- firstdraft.py
+++ firstdraft.py
@@ -1,11 +1,19 @@
+def greet_user():
+    print "Welcome to Follow Your Tastes."
+    print "We look forward to seeing where you will go!"
+
 def ask_for_info():
-    question = """/n
+    question = /n"""
     Which of the following food choices do you prefer?
          A. Spaghetti
          B. Bratwurst
          C. Lomein
-         D. Falafel/n : """
+         D. Falafel : """/n
     food_preference = (raw_input(question)).upper()
+
+    return food_preference
+
+def creates_prints_destination(food_preference):
 
     if food_preference == "A":
         print "Let's go to Italy!"
@@ -16,7 +24,8 @@ def ask_for_info():
     elif food_preference == "D":
         print "Let's go to Israel!"
 
-    next_question = (raw_input("Do you want to specify where to go? ")).lower()
+def ask_if_continue_to_region():
+    next_question = (raw_input("Do you want to go to a specific region? ")).lower()
 
     if next_question == "yes":
         specify_region()
@@ -38,6 +47,15 @@ def specify_region():
         within_Israel()
     else:
         print "Please choose a country that is on our list. "
-# ----------------------------------------------------------------
 
-ask_for_info()
+def ask_if_repeat_whole_process():
+# --------------------------------------------------------------
+
+greet_user
+
+playing = True
+while playing:
+    ask_for_info() = food_preference
+    creates_prints_destination(food_preference)
+    ask_if_continue_to_region()
+    ask_if_repeat_whole_process()
